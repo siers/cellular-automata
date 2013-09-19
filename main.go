2aa0cdda2daed5b23210c61a90ddad111e7cd859
siers <wimuan@gmail.com>
2013-09-19 15:53:53 +0300

    Initial commit.

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	_ "image/png"
+	"log"
+	"os"
+)
+
+var black = color.Gray{0}
+var white = color.Gray{255}
+
+func alive(n color.Color) bool {
+	r, _, _, _ := n.RGBA()
+	return r == 0
+}
+
+func countAlive(ns []color.Color) int {
+	m := 0
+
+	for _, n := range ns {
+		if alive(n) {
+			m++
+		}
+	}
+
+	return m
+}
+
+func evolvePixel(c color.Color, ns []color.Color) color.Color {
+	alive_n := countAlive(ns)
+
+	if alive_n == 3 || (alive(c) && alive_n == 2) {
+		return black
+	}
+
+	return white
+}
+
+func neighbours(p image.Point) []image.Point {
+	pts := make([]image.Point, 8)
+
+	pts = append(pts, image.Point{p.X - 1, p.Y - 1})
+	pts = append(pts, image.Point{p.X + 1, p.Y - 1})
+	pts = append(pts, image.Point{p.X - 1, p.Y + 1})
+	pts = append(pts, image.Point{p.X + 1, p.Y + 1})
+
+	pts = append(pts, image.Point{p.X, p.Y + 1})
+	pts = append(pts, image.Point{p.X, p.Y - 1})
+	pts = append(pts, image.Point{p.X - 1, p.Y})
+	pts = append(pts, image.Point{p.X + 1, p.Y})
+
+	return pts
+}
+
+func pixget(i image.Image, bounds image.Rectangle) func(x, y int) color.Color {
+	return func(x, y int) color.Color {
+		if image.Pt(x, y).In(bounds) {
+			return i.At(x, y)
+		} else {
+			return white
+		}
+	}
+}
+
+func evolve(input image.Image) image.Image {
+	var (
+		out   = image.NewGray(input.Bounds())
+		bound = out.Bounds()
+		at    = pixget(input, bound)
+		ns    = make([]color.Color, 0)
+	)
+
+	for i := bound.Min.X; i < bound.Max.X; i++ {
+		for j := bound.Min.Y; j < bound.Max.Y; j++ {
+			ns = nil
+			for _, n := range neighbours(image.Point{i, j}) {
+				ns = append(ns, at(n.X, n.Y))
+			}
+
+			out.Set(i, j, evolvePixel(at(i, j), ns))
+		}
+	}
+
+	return out
+}
+
+func save(i image.Image, nth int) {
+	name := "evolution.t" + fmt.Sprintf("%03d", nth) + ".png"
+
+	file, err := os.Create(name)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Creating: %s\n", name)
+	png.Encode(file, i)
+}
+
+func main() {
+	target, _, err := image.Decode(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; ; i++ {
+		save(target, i)
+		target = evolve(target)
+	}
+}
